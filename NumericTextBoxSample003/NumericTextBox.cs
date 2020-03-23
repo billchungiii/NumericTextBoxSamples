8b653d0e789a8ebd5926114ce4c244c50cb7dbd6
Bill Chung <bill_chungiii@hotmail.com>
2020-03-23 18:45:57 +0800

    Add NumericTextBox

---
+++ NumericTextBoxSample003/NumericTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NumericTextBoxSample003
+{
+    public class NumericTextBox : TextBox
+    {
+        protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, "^[0-9]*$");
+        }
+    }
+}
