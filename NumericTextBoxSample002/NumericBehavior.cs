dd0533508b960653abda7eead534809b47fa77b7
Bill Chung <bill_chungiii@hotmail.com>
2020-03-23 18:37:42 +0800

    Add new project - by Behavior

---
+++ NumericTextBoxSample002/NumericBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace NumericTextBoxSample002
+{
+    public class NumericBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewTextInput += OnPreviewTextInput;
+        }
+
+        private void OnPreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, "^[0-9]*$"); 
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewTextInput -= OnPreviewTextInput;
+        }
+    }
+}
